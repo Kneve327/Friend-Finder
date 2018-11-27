019c580b75d8fe56d37cbb376951346375b28114
Kneve327 <kneve327@gmail.com>
2018-11-27 12:30:19 -0500

    making sure files are added

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,35 +2,35 @@ var employeesArray = [{
     name: "Jim Halpert",
     photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjZlMa-2-PeAhXqt1kKHeBEDboQjRx6BAgBEAU&url=https%3A%2F%2Ftvline.com%2F2018%2F02%2F12%2Fthe-office-revival-john-krasinski-jim-video%2F&psig=AOvVaw1_xC52luABFS4tO01ikNtX&ust=1542828771504859",
     scores: [
-
+        1, 1, 1, 5, 1, 2, 3, 2, 2, 5
     ]
 },
 {
     name: "Pam Beesley",
     photo:"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiO6JC33OPeAhXwtlkKHaeWDV0QjRx6BAgBEAU&url=https%3A%2F%2Ftheofficeanalytics.wordpress.com%2F2017%2F09%2F23%2Fpam-beesly-isfj%2F&psig=AOvVaw23BHNagD1H2dofeJ1-l4qt&ust=1542829082310898",
     scores: [
-
+        5, 1, 1, 4, 5, 4, 3, 4, 4, 4
     ]
 },
 {
     name: "Michael Scott",
     photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi5neuD3ePeAhXJxFkKHTbVAa4QjRx6BAgBEAU&url=https%3A%2F%2Fwww.elitedaily.com%2Fp%2F10-secrets-about-the-office-that-even-the-biggest-michael-scott-fan-wont-know-8735316&psig=AOvVaw1NOPPCwKItwHELQhplSpMU&ust=1542829257136080",
     scores: [
-
+        1, 3, 1, 5, 1, 2, 5, 1, 1, 5
     ]
 },
 {
     name: "Dwight Schrute",
     photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiQj57b3ePeAhWExFkKHYZOC8EQjRx6BAgBEAU&url=https%3A%2F%2Fwww.themarysue.com%2Fnbc-office-spin-off-dwight-schrute%2F&psig=AOvVaw01LFILiqxvartoiS0dN1Hq&ust=1542829407655750",
     scores: [
-
+        5, 5, 5, 2, 3, 2, 3, 1, 3, 4
     ]
 },
 {
     name: "Angela Martin",
     photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiG-tmR3uPeAhUBjlkKHYO5AHIQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F490610953135439706%2F&psig=AOvVaw1ZCXZd5FwYcIdReII0ARyJ&ust=1542829487906179",
     scores: [
-
+        5, 5, 5, 1, 5, 1, 3, 1, 5, 2
     ]
 }
 
